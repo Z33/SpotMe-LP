8ae816bbf8552b1300549e2e9bdfb54121b779f0
Thomas Daugherty <z33studios@gmail.com>
2020-07-03 19:40:39 -0400

    - Netlify Form Test

--- src/components/formTest.js
+++ src/components/formTest.js
@@ -2,7 +2,13 @@ import React from "react"
 
 export default function FormGetStarted() {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>
           Your Name: <input type="text" name="name" />
